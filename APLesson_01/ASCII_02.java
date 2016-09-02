b559d30f702d66ec837fda8a57eb5da7fabbbbef
ayanhaque <ayan.haque09@gmail.com>
2016-09-02 13:34:29 -0700

    Added Files

---
+++ APLesson_01/ASCII_02.java
@@ -0,0 +1,8 @@
+public class ASCII_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" (\\__/)");
+		System.out.println("(=-.-=)");
+	}
+}
---
+++ APLesson_01/firstProg.java
@@ -0,0 +1,20 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+/* 		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println("  \"\"  \"\"   "); */
+		System.out.println(" _");
+		System.out.println("( (");
+		System.out.println("  ) )  .-```-.     n.-.n");
+		System.out.println("( (  /            \\ /  ,   ,  \\");
+		System.out.println(" \\      \\         =;    t    /= ");
+		System.out.println("  \\       |```.  `,--- `");
+		System.out.println("   /    //        |  ||");
+		System.out.println("  / _, ))       /_, )) ");
+		System.out.println("    ");
+		System.out.println(" (\\_/)");
+		System.out.println("( -_- )");
+	}
+}
