b5ababe65c646910b360ed2b66837866ae69d405
ayanhaque <ayan.haque09@gmail.com>
2016-10-27 13:11:00 -0700

    Update Box.java

--- APLesson_06/Box.java
+++ APLesson_06/Box.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-<<<<<<< HEAD
-<<<<<<< HEAD
 public class Box
 {
    public static void main(String[]args)
@@ -14,26 +12,3 @@ public class Box
         }     
     }
 }
-  
-=======
-=======
->>>>>>> origin/master
-public class Reverse
-{
-	public static void main(String[]args)
-	{
-		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter a word: ");
-		String word = kb.next();
-		
-		for(int i = 7; i >= word.length(); i--)
-		{
-			System.out.println(word.substring(i,0));
-		}
-	}
-<<<<<<< HEAD
-}
->>>>>>> origin/master
-=======
-}
->>>>>>> origin/master
