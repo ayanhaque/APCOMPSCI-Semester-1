a471f6113800b39414a9d3721bf3ee5b7af9a8c3
ayanhaque <ayan.haque09@gmail.com>
2016-11-03 23:08:44 -0700

    finish

--- APLesson_06/Song.java
+++ APLesson_06/Song.java
@@ -1,20 +1,19 @@
 public class Song
 {
-    public static void main(String[]args)
-    {
-        
-        sing("Na", 4);
-        sing("Na", 4);
-        sing("Hey", 3);
-        sing("Goodbye!", 1);
-    }
-    public static void sing (String word, int times);
-    {
-        String word1 = word;
-        
-        for(int i = 0; i <= word.substring(); i--)
-        {
-            System.out.println(word);
-        }
-    }    
+	public static void main(String[]args)
+	{
+		sing("Na", 4);
+		sing("Na", 4);
+		sing("Hey", 3);
+		sing("Goodbye!", 1);
+	}
+	
+	public static void sing(String word, int times)
+	{
+		for(int i = 1; i <= times; i++)
+		{
+			System.out.print(word + "\t");
+		}
+		System.out.println();
+	}
 }
