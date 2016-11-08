ac0941bdcfcf08216f6455ce4e6ed07f418bde29
ayanhaque <ayan.haque09@gmail.com>
2016-11-08 06:36:54 -0800

    Update ReplaceAts.java

--- APLesson_07/ReplaceAts.java
+++ APLesson_07/ReplaceAts.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 public class ReplaceAts
 {
-	static String sentence;
-	
-	public static void main(String[]args);
-	{
-		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter a sentence: ");
-		sentence = kb.nextLine();
-		
-		replace(String sentence);
-	}
-	public static void replace(String sentence)
-	{
-		while(sentence.indexOf("a") >= 0)
-		{
-			sentence = sentence.substring(0, sentence.indexOf("a")) + sentence.substring(sentence.indexOf("a") + @);
-		}
-		System.out.println(sentence);
-	}
+    static String sentence;
+    
+    public static void main(String[]args)
+    {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please enter a sentence: ");
+        sentence = kb.nextLine();
+        
+        System.out.print(replace(sentence));
+    }
+    
+    public static String replace(String sentence)
+    {
+        while(sentence.indexOf("a") >= 0)
+        {
+            sentence = sentence.substring(0,sentence.indexOf("a")) + "@" + sentence.substring(sentence.indexOf("a")+1, sentence.length());
+        }
+        return sentence;
+    }
 }
