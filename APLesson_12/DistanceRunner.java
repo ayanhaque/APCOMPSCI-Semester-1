f3f71ee1ac02afbd3c9bd1ae3f393945cd9cdeaf
ayanhaque <ayan.haque09@gmail.com>
2017-01-09 22:36:48 -0800

    finished

---
+++ APLesson_12/DistanceRunner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class DistanceRunner
+{
+   public static void main(String[]args)
+   {
+       Scanner kb = new Scanner(System.in);
+       
+       System.out.println("Please enter the x1 coordinate: ");
+       int x1 = kb.nextInt();
+       System.out.println("Please enter the y1 coordinate: ");
+       int y1 = kb.nextInt();
+       System.out.println("Please enter the x2 coordinate: ");
+       int x2 = kb.nextInt();
+       System.out.println("Please enter the y2 coordinate: ");
+       int y2 = kb.nextInt();
+       
+       
+       Distance object = new Distance(x1, y1, x2, y2);
+       System.out.println("The distance between points (" + object.getXOne() + ", " + object.getYOne() + ") and (" + object.getXTwo() + ", " + object.getYTwo() + ") is " + object.getDist());
+   }
+}
