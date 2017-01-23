c8f57f6f4d86279b228dc897c87c9cbe90996949
ayanhaque <ayan.haque09@gmail.com>
2017-01-22 23:00:39 -0800

    Create InventoryRunner.java

---
+++ APLesson_12/APLesson_12_2/InventoryRunner.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class InventoryRunner
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter the manufacturer of the itme:");
+		String manufacturer = kb.next();
+		System.out.println("Enter the name of the item:");
+		String name = kb.next();
+		
+		System.out.println("Will you be entering category and price information?");
+		String info = kb.next();
+		if(info.equals("n"))
+		{
+			Inventory object = new Inventory(manufacturer, name);
+			System.out.println(object);
+		}
+		if(info.equals("y"))
+		{
+			System.out.println("Enter the category:");
+			String category = kb.next();
+			System.out.println("Enter the price:");
+			int price = kb.nextInt();
+			Inventory object1 = new Inventory(manufacturer, name, category, price);
+			System.out.println(object1);
+		}
+	}
+}
