e4204da297c5955ca34add3d07c16cae8cd77a9a
ayanhaque <ayan.haque09@gmail.com>
2017-01-22 22:51:20 -0800

    Update UserRunner.java

--- APLesson_12/APLesson_12_2/UserRunner.java
+++ APLesson_12/APLesson_12_2/UserRunner.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
-public UserRunner
+public class UserRunner
 {
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Enter the first name: ");
+		System.out.println("Enter your first name: ");
 		String firstname = kb.next();
-		System.out.println("Enter the last name: ");
+		System.out.println("Enter your last name: ");
 		String lastname = kb.next();
 		System.out.println("Would you like to use a public avatar? (y for yes, n for no) ");
 		String avatar = kb.next();
-		if(avatar.equals("y"))
-		{
-			UserRunner ph = new User(first, last);
-		}
 		if(avatar.equals("n"))
+		{
+			User pH1 = new User(firstname, lastname);
+			System.out.println(pH1);
+		}
+		if(avatar.equals("y"))
 		{
 			System.out.println("What avatar would you like? ");
 			String avatar1 = kb.next();
-			UserRunner ph = new User(first, last, avatar);
+			User pH2 = new User(firstname, lastname, avatar1);
+			System.out.println(pH2);
 		}
 	}
 }
