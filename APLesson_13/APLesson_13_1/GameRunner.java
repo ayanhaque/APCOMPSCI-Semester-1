09008e9ab019a5e7800f9ad54d502b2567a54d03
ayanhaque <ayan.haque09@gmail.com>
2017-02-12 23:59:14 -0800

    lesson 13.1

---
+++ APLesson_13/APLesson_13_1/GameRunner.java
@@ -0,0 +1,15 @@
+package APLesson_13_1;
+public class GameRunner
+{
+    public static void main(String[]args)
+    {
+        XBox xbox = new XBox("XBox");
+        System.out.println(xbox);
+        
+        PlayStation ps = new PlayStation("PlayStation");
+        System.out.println(ps);
+        
+        PC pc = new PC("PC");
+        System.out.println(pc);
+    }
+}
---
+++ APLesson_13/APLesson_13_1/PC.java
@@ -0,0 +1,23 @@
+package APLesson_13_1;
+public class PC extends GameSystem
+{
+    public PC()
+    {
+        super();
+    }
+    public PC(String p)
+    {
+        super(p);
+    }
+    
+    public String systemInput()
+    {
+        return "Keyboard and Mouse";
+    }
+    public String toString()
+    {
+        return "Platform: " + super.getPlatform() + 
+        "\n Serial #: " + super.getSerialNo() +
+        "\n System Input: " + systemInput();
+    }
+}
