0b0638b347a3e6a936ae158bcedff037ba84ab0d
ayanhaque <ayan.haque09@gmail.com>
2017-02-13 12:42:53 -0800

    Update GameSystem.java

--- APLesson_13/APLesson_13_1/GameSystem.java
+++ APLesson_13/APLesson_13_1/GameSystem.java
@@ -1,27 +1,26 @@
 public class GameSystem
 {
-    private String platform;
-    private int serialNo;
+	private String platform;
+	private int serialNo;
     
-    public GameSystem()
-    {
-        this.platform = "";
+	public GameSystem()
+	{
+		this.platform = "";
 		this.serialNo = 0;
-    }
+	}
     
-    public GameSystem(String p)
-    {
-        this.platform = p;
-        this.serialNo = (int)(Math.random() * 10000000) + 1;
-    }
-    
-    public String getPlatform()
+	public GameSystem(String p)
 	{
+        	this.platform = p;
+		this.serialNo = (int)(Math.random() * 10000000) + 1;
+	}
+    
+    	public String getPlatform()
+    	{
 		return platform;
-	}
-	
-	public int getSerialNo()
-	{
+    	}
+   	public int getSerialNo()
+    	{
 		return serialNo;
-	}
+    	}
 }
