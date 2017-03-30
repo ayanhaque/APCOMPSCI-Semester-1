185eb164dacb256d33620640dc0ec276f91e3516
ayanhaque <ayan.haque09@gmail.com>
2017-03-29 19:00:03 -0700

    Update GMC.java

--- APLesson_14/APLesson_14.1/GMC.java
+++ APLesson_14/APLesson_14.1/GMC.java
@@ -4,32 +4,12 @@ public class GMC extends Car
     private double x, y;
     
     public GMC()
-    {
-        x = 0;
-        y = 0;
-    }
+    {}
     
     public GMC(double xx, double yy)
     {
-        x = xx;
-        y = yy;
+        location[0] += xx;
+		location[1] += yy;
     }
-    
-    public void move(double xx, double yy)
-    {
-        x += xx;
-        y += yy;
-    }
-    
-    public int getID()
-    {
-        return (int)(Math.random() * 1000000) + 1;
-    }
-    public double [] getLoc()
-    {
-        double [] loc = new double[2];
-        loc[0] = x;
-        loc[1] = y;
-        return loc;
-    }
+
 }
