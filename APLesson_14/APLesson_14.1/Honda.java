bee89d99898a23da83d77bb1df14873d141b3986
ayanhaque <ayan.haque09@gmail.com>
2017-04-04 08:07:51 -0700

    Update Honda.java

--- APLesson_14/APLesson_14.1/Honda.java
+++ APLesson_14/APLesson_14.1/Honda.java
@@ -1,30 +1,12 @@
 import java.util.Arrays;
 public class Honda extends Car
 {
-    private double [] locate;
-
     public Honda()
-    {
-        locate = new double[2];
-    }
+	{}
     
     public Honda(double [] l)
     {
-        locate = new double[2];
         locate = l;
     }
     
-    public void move(double xx, double yy)
-    {
-        locate [0] += xx;
-        locate [1] += yy;
-    }
-    public int getID()
-    {
-        return (int)(Math.random() * 1000000) + 1;
-    }
-    public double [] getLoc()
-    {
-        return locate;
-    }
 }
