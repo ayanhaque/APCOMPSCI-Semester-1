636b39f06c85958dbbf88ee229cab579ba8c21a4
ayanhaque <ayan.haque09@gmail.com>
2017-04-04 08:10:05 -0700

    Update Toyota.java

--- APLesson_14/APLesson_14.1/Toyota.java
+++ APLesson_14/APLesson_14.1/Toyota.java
@@ -4,31 +4,13 @@ public class Toyota extends Car
     private String loc;
     
     public Toyota()
-    {
-        loc = "";
-    }
+    {}
+	
     public Toyota(String lcte)
     {
-        loc = lcte;
+    	String[] locate = loc.split(", ");
+    	location[0] = Double.parseDouble(locate[0]);
+    	location[1] = Double.parseDouble(locate[1]);
     }
-    public void move (double x, double y)
-    {
-        String [] locate = loc.split(", ");
-		double [] lcte = new double[2];
-		lcte[0] = Double.parseDouble(locate[0]) + x;
-		lcte[1] = Double.parseDouble(locate[1]) + y;
-		loc = "" + lcte[0] + ", " + lcte[1];
-    }
-    public int getID()
-    {
-        return (int)(Math.random() * 1000000) + 1;
-    }
-    public double [] getLoc()
-    {
-        double [] location = new double[2];
-        String [] locate = loc.split(", ");
-        location [0] = Double.parseDouble(locate[0]);
-        location [1] = Double.parseDouble(locate[1]);
-        return location;
-    }
+
 }
