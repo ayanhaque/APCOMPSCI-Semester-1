3529b049640da7ccc1c8740694386959f76952fd
ayanhaque <ayan.haque09@gmail.com>
2017-04-04 08:43:22 -0700

    Update Driver.java

--- APLesson_14/APLesson_14.2/Driver.java
+++ APLesson_14/APLesson_14.2/Driver.java
@@ -12,46 +12,52 @@ public class Driver
        locate.add(new GMC(3, 8));
 
        double[] home = {0, 0};
-
-       String printout = "\n\n" +
-               "==========================" + "\nStarting locations...";
-
-        for (Location l : locate)
-		{
-		   printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
-		}
+	String printout = "\n\n" +
+				"==========================" + "\nStarting locations...";
+	for (Location l : locate)
+	{
+		printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
+	}
 
         printout += "\n\n" + "==========================" +
-                   "\nDistance from home...";
+						"\nDistance from home...";
+						
+	System.out.println(printout);
 
         for (Location l : locate)
-		{
-           printout += "\nDistance for " + l.getID() + ": (" + getDistance(l.getLoc(), home)+ ")";
-		}
+	{
+		System.out.printf("\nDistance for %d : (%.2f)", l.getID(), getDistance(l.getLoc(), home));
+	}
 
-        System.out.println(printout);
+        
 		
-		String Printout = "\n\n" + "==============================";
-		for(Location l : locate)
-		{
-			l.move(one, two);
-			Printout += "\n\n After " + l.getID() + " Moved (" + String.format("(%.2f, %.2f)", one, two) + ")" + 
-						"\n New Location: (" + getLocation(l.getLoc()) + ")";
-		}
-		for(Location l : locate)
-		{
-			Printout += "\n\n Distance for " + l.getID() + ": (" + getDistance(l.getLoc(), home) + ")";
-		}
-		System.out.println(Printout);
-    }
-
-    public static double getDistance(double [] car, double [] home)
+	String printout1 = "\n\n" +
+				"==========================";
+				
+	System.out.println(printout1);
+				
+	for(Location l : locate)
 	{
+		l.move(one, two);
+		System.out.printf("\nAfter %d Moved (%.2f, %.2f)\nNew Location: (%s)", l.getID(), one, two, getLocation(l.getLoc()));
+	}
+		
+	System.out.println();
+		
+	for (Location l : locate)
+	{
+		System.out.printf("\nDistance for %d: (%.2f)", l.getID(), getDistance(l.getLoc(), home));
+	}		
+		
+   }
+
+   public static double getDistance(double[] car, double[] home)
+   {
        return Math.sqrt((Math.pow(car[0] - home[0], 2)+ Math.pow(car[1] - home[1], 2)));
-	}
+   }
 
-    public static String getLocation(double [] loc)
-	{
-       return loc[0] + ", " + loc[1];
-	}
+   public static String getLocation(double[] loc)
+   {
+       return String.format("%.2f, %.2f", loc[0], loc[1]);
+   }
 }
