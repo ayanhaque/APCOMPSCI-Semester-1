65adab624675ff59578955915e5de9a800f4c092
ayanhaque <ayan.haque09@gmail.com>
2017-04-04 08:37:15 -0700

    Update GMC.java

--- APLesson_14/APLesson_14.2/GMC.java
+++ APLesson_14/APLesson_14.2/GMC.java
@@ -1,13 +1,12 @@
 import java.util.Arrays;
 public class GMC extends Car
 {
-    private double x, y;
-    
+    public GMC()
+    {}
     public GMC(double x, double y)
     {
-        location = new double [2];
-		location[0] = x;
-		location[1] = y;
+		location[0] += x;
+		location[1] += y;
     }
    
 }
